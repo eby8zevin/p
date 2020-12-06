40a2e70baf79a7214d97d7f2dcf1f0bf49f2aefb
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-06 19:39:35 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -57,7 +57,7 @@ include 'koneksi.php'
     
     $ambil = "SELECT * FROM data_mhs WHERE NIM='$NIM'";
     $query=sqlsrv_query($conn, $ambil) or die(sqlsrv_errors());
-    if (sqlsrv_num_rows($query)==true) {
+    if (sqlsrv_num_rows($query)==1) {
      echo "<script>alert('Gagal: NIM sudah ada');</script>";
      echo "<meta http-equiv='refresh' content='0;url=tambah.php?datadisimpan=gagal'>";
     } else {
