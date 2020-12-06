24acafa3ed73544bc73f3fdf3fbf9c37347f4b79
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-06 12:14:59 +0700

    Create tambah.php

---
+++ tambah.php
@@ -0,0 +1,66 @@
+<?php
+include 'koneksi.php'
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Tambah Data</title>
+</head>
+<body>
+ <div align="center">
+  <h3>Simpan Data SQL Server dengan PHP<br>Form Tambah Data</h3>
+  <table >
+   <form method="POST" >
+    <tr>
+     <td>NIM</td>
+     <td><input type="text" name="NIM" id="NIM"></td>
+    </tr>
+    <tr>
+     <td>Nama</td>
+     <td><input type="text" name="Nama" id="Nama"></td>
+    </tr>
+    <tr>
+     <td>Prodi</td>
+     <td>
+      <select name="Prodi" id="Prodi">
+       <option disabled="" selected="">-Pilih-</option>
+       <option value="Teknik Informatika">Teknik Informatika</option>
+       <option value="Manajemen Informatika">Manajemen Informatika</option>
+       <option value="Sistem Informasi">Sistem Informasi</option>
+      </select>
+     </td>
+    </tr>
+    <tr>
+     <td>Tanggal Bulan Tahun</td>
+     <td><input type="text" name="TBT" id="TBT" placeholder="DD-MM-YYYY"></td>
+    </tr>
+    <tr>
+     <td></td>
+     <td>
+      <input type="submit" name="simpan" value="Simpan">
+      <a href="index.php">Kembali</a>
+     </td>
+    </tr>
+   </form>
+  </table>
+  <?php
+   //eksekusi simpan data
+   if (isset($_POST['simpan'])) {
+    # code...
+    $NIM=$_POST['NIM'];
+    $Nama=$_POST['Nama'];
+    $Prodi=$_POST['Prodi'];
+    $TBT=$_POST['TBT'];
+    $sql="INSERT INTO data_mhs (NIM, Nama, Prodi, TanggalBulanTahun) VALUES ('$NIM', '$Nama', '$Prodi', '$TBT')";
+    $query=sqlsrv_query($conn,$sql) or die(sqlsrv_errors());
+    if ($query) {
+     //redirect ke halaman index
+     echo "<script>alert('Data berhasil di tambahkan!');history.go(-1);</script>";
+     //header("Location:index.php");
+    }
+   }
+  ?>
+ </div>
+</body>
+</html>
