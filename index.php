15d51ca18839f0469e3583778d6abdfd303b9e0c
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-06 11:48:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ include 'koneksi.php';
       <td><?php echo $data['Nama']; ?></td>
       <td><?php echo $data['Prodi']; ?></td>
       <td><?php echo $data['TanggalBulanTahun']; ?></td>
-      <td>
+      <td style="float: center;">
         <a href="edit.php?id=<?php echo $data['ID']; ?>" style="float: left;">Edit</a> |
         <a href="hapus.php?id=<?php echo $data['ID']; ?>" style="float: right;" onClick="javascript: return confirm('Apakah anda yakin?');">Hapus</a>
       </td>
