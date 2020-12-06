01577ef1f9735028c19fda44ac2f84c4941848f8
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-06 10:14:37 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$serverName = "stmik-yadika-bangil.database.windows.net"; //serverName\instanceName
+
+// Since UID and PWD are not specified in the $connectionInfo array,
+// The connection will be attempted using Windows Authentication.
+$connectionInfo = array( "Database"=>"uas-aplikasimobile7-db", "UID"=>"stmikyadikabangil", "PWD"=>"!5tm1ky4d1k4b4n91l#");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
