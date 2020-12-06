cbf68a670d05dbb182c4ee96cbe6e0a9fa61fbc1
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-06 13:48:00 +0700

    Create hapus.php

---
+++ hapus.php
@@ -0,0 +1,16 @@
+<?php
+ // panggil file koneksi
+ include 'koneksi.php';
+ 
+ //ambil data id dari parameter
+ $ID=$_GET['ID'];
+
+ $sql="DELETE FROM data_mhs WHERE ID='$ID'";
+ $query=sqlsrv_query($conn,$sql) or die(sqlsrv_errors());
+ if ($query) {
+  //redirect ke halaman index
+  echo "<script>alert('Data berhasil dihapus!');history.go(-1);</script>";
+  //header("Location:index.php");
+ }
+ 
+?>
