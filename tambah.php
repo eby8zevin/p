14973e7071b398f16c3020ba6eeaf1f9ce8f9bc0
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-07 09:12:44 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -55,10 +55,10 @@ include 'koneksi.php'
     $Prodi=$_POST['Prodi'];
     $TBT=$_POST['TBT'];
     
-    $ambil = "SELECT COUNT (*) FROM data_mhs WHERE NIM='$NIM'";   
+    $ambil = "SELECT COUNT(*) FROM data_mhs WHERE NIM='$_POST[NIM]'";   
     $query=sqlsrv_query($conn, $ambil) or die(sqlsrv_errors());
     
-    if (sqlsrv_num_rows($query)>0) {
+    if (sqlsrv_num_rows($query)>=1) {
      echo "<script>alert('Gagal: NIM sudah ada.');</script>";
      echo "<meta http-equiv='refresh' content='0;url=tambah.php?datadisimpan=gagal'>";
     } else {
