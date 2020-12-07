0ec4d1be4b32b469c4b973c622e27d2d6d2397c8
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-07 11:00:12 +0700

    Create api-view.php

---
+++ API/api-view.php
@@ -0,0 +1,26 @@
+<?php
+include 'koneksi.php';
+
+//syntax melihat semua record data yang ada di tabel data_mhs
+$sql = "SELECT * FROM data_mhs";
+
+//eksekusi query diatas
+$query = mysqli_query($sql);
+while($data=mysqli_fetch_array($query)){
+  $item[] = array (
+    "ID_datamhs" => $data['ID_datamhs'],
+    "Nama" => $data['Nama'],
+    "Program Studi" => $data['Prodi'],
+    "Tanggal Bulan Tahun" => $data['TanggalBulanTahun']
+  );
+}
+
+//menampung data yang dihasilkan
+$json = array (
+  'result' => 'Succes',
+  'item' => $item
+ );
+ 
+//merubah data kedalam bentuk JSON
+echo json_encode($json);
+?>
