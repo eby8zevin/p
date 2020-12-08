e45fd9853842246023698de6657ae8304e810cb1
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-09 05:55:17 +0700

    Update api-view.php

--- API/api-view.php
+++ API/api-view.php
@@ -1,52 +1,16 @@
-<?php
-include 'koneksi.php';
-
-//syntax melihat semua record data yang ada di tabel data_mhs
-$sql = "SELECT * FROM data_mhs";
-
-//eksekusi query diatas
-$query = sqlsrv_query($conn, $sql);
-while($data=sqlsrv_fetch_array($query)){
-  $item[] = array (
-    'ID_datamhs' => $data['ID_datamhs'],
-    'NIM' => $data['NIM'],
-    'Nama' => $data['Nama'],
-    'Prodi' => $data['Prodi'],
-    'TanggalBulanTahun' => $data['TanggalBulanTahun']
-  );
-}
-
-//menampung data yang dihasilkan
-$json = array (
-  'result' => 'Succes',
-  'item' => $item
- );
- 
-//merubah data kedalam bentuk JSON
-echo json_encode($json);
-
-
-//pdo
-// $response = [];
-
-// $query = $conn->prepare("SELECT * FROM data_mhs ORDER BY NIM ASC");
-// $query->execute();
-
-// if ($query->rowCount() == 0) {
-//     $response['status'] = false;
-//     $response['message'] = "Data tidak ditemukan";
-// } else {
-//     while ($data = $query->fetch()) {
-//         $response[]= [
-//           'ID_datamhs' => $data['ID_datamhs'],
-//           'NIM' => $data['NIM'],
-//           'Nama' => $data['Nama'],
-//           'Program_Studi' => $data['Prodi'],
-//           'Tanggal_Bulan_Tahun' => $data['TanggalBulanTahun']
-//        ];
-//     }
-// }
-
-// $json = json_encode($response, JSON_PRETTY_PRINT);
-// echo $json;
+<?php 
+	include "koneksi.php";
+	
+	$query = mysqli_query("SELECT * FROM data_mhs");
+	
+	$json = array();
+	
+	while($row = mysqli_fetch_assoc($query)){
+		$json[] = $row;
+	}
+	
+	echo json_encode($json);
+	
+	mysqli_close($conn);
+	
 ?>
