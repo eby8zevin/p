6a500ac89612c43c5207faa6db324783af91202f
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-12-25 15:34:22 +0700

    251222

--- README.md
+++
@@ -1,3 +0,0 @@
-# AdminSekolah Presensi
-
-[![Android CI](https://github.com/eby8zevin/p/actions/workflows/android.yml/badge.svg)](https://github.com/eby8zevin/p/actions/workflows/android.yml)
--- app/src/main/java/com/ekosp/indoweb/adminsekolah/model/ListDataKehadiran.kt
+++
@@ -1,7 +0,0 @@
-package com.ekosp.indoweb.adminsekolah.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-class ListDataKehadiran(val tgl_hadir: String, val status: String) : Parcelable
