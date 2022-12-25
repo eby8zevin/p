3f81f3bb348f92466456c279e54c7d5159ae7cdb
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-12-25 14:50:32 +0700

    AS

--- app/src/main/java/com/ekosp/indoweb/adminsekolah/helper/ApiClient.java
+++ app/src/main/java/com/ekosp/indoweb/adminsekolah/helper/ApiClient.java
@@ -11,10 +11,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    //public static final String BASE_URL = "https://m.adminsekolah.net/rest-api/"; //server Real
-    public static final String BASE_URL_DEMO = "https://demo.adminsekolah.net/rest-api/"; //server Demo
-
-    public static final String BASE_URL = "http://192.168.1.205/adminsekolah-presensi-api/rest-api/";
+    public static final String BASE_URL = "https://m.adminsekolah.net/rest-api/";
+    public static final String BASE_URL_DEMO = "https://demo.adminsekolah.net/rest-api/";
 
     // File upload url (replace the ip with your server address)
     public static final String FILE_UPLOAD_URL = BASE_URL + "absen_datangpulang.php";
